87f09ad36987072e3903327eff9b4ed46c2fc7d8
maheshwaghmare <mwaghmare7@gmail.com>
2017-01-22 16:05:14 +0530

    Removed 3rd party assets."

--- inc/assets/unminified/js/customizer.js
+++ inc/assets/unminified/js/customizer.js
@@ -6,50 +6,7 @@
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
  */
 
-function bhari_colors_live_update( id, selector, property, default_value ) {
-	default_value = typeof default_value !== 'undefined' ? default_value : 'initial';
-	wp.customize( 'generate_settings[' + id + ']', function( value ) {
-		value.bind( function( newval ) {
-			newval = ( '' !== newval ) ? newval : default_value;
-			if ( jQuery( 'style#' + id ).length ) {
-				jQuery( 'style#' + id ).html( selector + '{' + property + ':' + newval + ';}' );
-			} else {
-				jQuery( 'head' ).append( '<style id="' + id + '">' + selector + '{' + property + ':' + newval + '}</style>' );
-				setTimeout(function() {
-					jQuery( 'style#' + id ).not( ':last' ).remove();
-				}, 1000);
-			}
-		} );
-	} );
-}
-
-function bhari_classes_live_update( id, classes, selector, prefix ) {
-	classes = typeof classes !== 'undefined' ? classes : '';
-	prefix = typeof prefix !== 'undefined' ? prefix : '';
-	wp.customize( 'generate_settings[' + id + ']', function( value ) {
-		value.bind( function( newval ) {
-			jQuery.each( classes, function( i, v ) {
-				jQuery( selector ).removeClass( v );
-			});
-			jQuery( selector ).addClass( prefix + newval );
-		} );
-	} );
-}
-
 ( function( $ ) {
-
-	
-	/** 
-	 * Link color hover
-	 * Empty:  initial
-	 */
-	bhari_colors_live_update( 'link_color_hover', 'a:hover', 'color', 'initial' );
-
-	/** 
-	 * Content layout
-	 */
-	bhari_classes_live_update( 'content_layout_setting', [ 'one-container', 'separate-containers' ], 'body' );
-
 
 	/** 
 	 * Container width for Page
